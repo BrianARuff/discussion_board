c609a542ce6a46e948d96e09165fdd46ffc888c4
BrianARuff <briananthonyruff@gmail.com>
2021-07-11 05:43:35 -0400

    connect to prod database

--- dist/config/db.js
+++ dist/config/db.js
@@ -2,13 +2,27 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 var pg_1 = require("pg");
-var pool = new pg_1.Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: 5432,
-});
+var pool;
+if (process.env.ENV === 'production') {
+    pool = new pg_1.Pool({
+        user: process.env.PROD_DB_USER,
+        password: process.env.PROD_DB_PASS,
+        host: process.env.PROD_DB_HOST,
+        port: process.env.PROD_DB_PORT,
+        database: process.env.PROD_DB_NAME,
+        max: 25,
+        ssl: true,
+    });
+}
+else {
+    pool = new pg_1.Pool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: 5432,
+    });
+}
 pool.connect();
 exports.default = pool;
 //# sourceMappingURL=db.js.map
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,27 @@
 import 'dotenv/config';
 import { Pool } from 'pg';
 
-const pool = new Pool({
-   host: process.env.DB_HOST,
-   user: process.env.DB_USER,
-   password: process.env.DB_PASSWORD,
-   database: process.env.DB_NAME,
-   port: 5432,
-});
+let pool: Pool;
+
+if (process.env.ENV === 'production') {
+   pool = new Pool({
+      user: process.env.PROD_DB_USER,
+      password: process.env.PROD_DB_PASS,
+      host: process.env.PROD_DB_HOST,
+      port: process.env.PROD_DB_PORT,
+      database: process.env.PROD_DB_NAME,
+      max: 25,
+      ssl: true,
+   } as any);
+} else {
+   pool = new Pool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: 5432,
+   });
+}
 
 pool.connect();
 
