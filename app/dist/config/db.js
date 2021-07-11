c1f6bac0f2a3d5719dfb57dc67090a0fb089d05b
BrianARuff <briananthonyruff@gmail.com>
2021-07-11 19:19:03 -0400

    server side

--- app/dist/config/db.js
+++ app/dist/config/db.js
@@ -3,11 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 var pg_1 = require("pg");
 var pool = new pg_1.Pool({
-    database: process.env.LOCAL_DB_NAME,
-    user: process.env.LOCAL_DB_USER,
-    password: process.env.LOCAL_DB_PASS,
-    host: process.env.LOCAL_DB_HOST,
-    port: 5432,
+    database: process.env.PROD_DB_NAME,
+    user: process.env.PROD_DB_USER,
+    password: process.env.PROD_DB_PASS,
+    host: process.env.PROD_DB_HOST,
+    port: process.env.PROD_DB_PORT,
+    max: 25,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 pool.connect();
 exports.default = pool;
--- app/src/routes/dasbohard.ts
+++ app/src/routes/dasbohard.ts
@@ -1,5 +1,24 @@
 import express from 'express';
+import database from '../config/db';
+import authorization from '../utils/authorization';
+import { Response } from 'express';
 
 const dashBoardRouter = express.Router();
+
+dashBoardRouter.get(
+   '/',
+   authorization as any,
+   async (req: any, res: Response) => {
+      try {
+         const user = await database.query(
+            'SELECT user_id, user_name, user_email, user_created_at, user_updated_at FROM users WHERE user_id = $1',
+            [req.user]
+         );
+         res.json(user.rows[0]);
+      } catch (error) {
+         res.status(500).json(error);
+      }
+   }
+);
 
 export default dashBoardRouter;
